7a7d8c1e901648c121f568580288fba481a38bd9
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-10-14 16:06:52 +0200

    Fix filterform None if get_queryset not called (fixes #847)
    Conflicts:
    
    	mapentity/static/mapentity/Leaflet.label

--- mapentity/tests/test_functional.py
+++ mapentity/tests/test_functional.py
@@ -208,6 +208,12 @@ class MapEntityTest(TestCase):
         response = self.client.get(obj.get_update_url())
         self.assertEqual(response.status_code, 302)
 
+    def test_formfilter_in_list_context(self):
+        if self.model is None:
+            return  # Abstract test should not run
+        response = self.client.get(self.model.get_list_url())
+        self.assertTrue(response.context['filterform'] is not None)
+
 
 class MapEntityLiveTest(LiveServerTestCase):
     model = None
