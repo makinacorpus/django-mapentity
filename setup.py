be4c08e320ae9ec39851dac07e3cbfdf8235771b
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-15 09:37:38 +0200

    Back to development: 1.2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='1.2.5',
+    version='1.2.6.dev0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
