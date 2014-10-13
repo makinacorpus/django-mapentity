746c9e33983af18ae0ab6f2bf00176e8617f2e18
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-10-13 12:18:36 +0200

    Preparing release 1.8.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='1.8.4.dev0',
+    version='1.8.4',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
