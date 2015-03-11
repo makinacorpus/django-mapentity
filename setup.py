93962bb16c32adfe1cb8eb855ed68d6cc782498f
Gael UTARD <gael.utard@kisis.fr>
2015-03-11 12:22:06 +0100

    Back to development

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='1.15.1',
+    version='1.15.2.dev0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
