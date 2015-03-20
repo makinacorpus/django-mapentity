a995323a81e7ec17b347e6f36f0d448279565f70
Gael UTARD <gael.utard@kisis.fr>
2015-03-20 17:18:11 +0100

    Preparing release 1.16.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='1.15.3.dev0',
+    version='1.16.0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
