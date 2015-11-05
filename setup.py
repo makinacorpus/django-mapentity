f5ae591165c7e697782572d05cf5c8c91fb6b8f5
Laurent Saint-Félix <anaethelion@gmail.com>
2015-11-05 16:11:58 +0100

    Fix setup.py, forgot versionning for pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='2.6.1.dev0',
+    version='2.6.1',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
