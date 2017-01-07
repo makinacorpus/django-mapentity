c3485eebb273cab38cb0e290551cf217885d73f2
Gaël UTARD <gael.utard@makina-corpus.com>
2017-01-07 15:23:49 +0100

    Add initial migration

---
+++ mapentity/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import mapentity.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('admin', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='LogEntry',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=(mapentity.models.MapEntityMixin, 'admin.logentry'),
+        ),
+    ]
