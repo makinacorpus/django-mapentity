40b024de20076bc4d921a30b1c0efcfd9faa2a16
J-E Castagnede <j.e.castagnede@gmail.com>
2017-03-23 18:30:24 +0100

    preparing release 3.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='3.0.2',
+    version='3.1.0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
