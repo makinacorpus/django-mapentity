4e27184e59ce973fcc66867fbf369d76d366bf42
Timothee <timothee.de-montety@makina-corpus.com>
2018-04-13 14:22:29 +0200

    Change version in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--- Install Ubuntu packages:"
-sudo apt -y install python-virtualenv build-essential python-dev libgdal-dev
+sudo apt -y install python-virtualenv build-essential python3-dev libgdal-dev
 if [ "`lsb_release -rs`" = "14.04" ]; then
     sudo apt -y install spatialite-bin
 else
@@ -10,7 +10,7 @@ fi
 sudo apt -y install libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
 echo "--- Create virtualenv:"
-[ -f env/bin/python ] || virtualenv env
+[ -f env/bin/python3 ] || virtualenv -p python3 env
 
 echo "--- Install python requirements:"
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
@@ -27,6 +27,6 @@ fi
 ./env/bin/pip install -r dev-requirements.txt
 
 echo "--- Setup:"
-./env/bin/python setup.py develop
+./env/bin/python3 setup.py develop
 
 echo "--- Done"
