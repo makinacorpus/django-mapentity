0267a78e97c594e7c2c5f62881839454abd9f253
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-11-08 17:56:50 +0100

    Preparing release 4.4.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='4.4.1.dev0',
+    version='4.4.1',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
