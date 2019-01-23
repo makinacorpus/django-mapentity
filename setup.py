89d24d815a3da872bd8c9b4ae4ed0d6b2027d60a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-23 15:42:50 +0000

    Back to development

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requires = [
 
 setup(
     name='mapentity',
-    version='4.4.7',
+    version='4.4.8.dev0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
