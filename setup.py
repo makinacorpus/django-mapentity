5418aa2dec2947e50aa711d9a0125214f2aad4ae
Gaël UTARD <gael.utard@makina-corpus.com>
2019-11-25 16:58:41 +0100

    Preparing release 6.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='mapentity',
-    version='6.0.3.dev0',
+    version='6.0.3',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-mapentity',
