29f0d551df2a44d1493697d9d2be8a6327daba7d
Bastien Potiron <merbatop@courriel.fr.nf>
2019-12-27 16:28:55 +0100

    [FIX] failing import of re_path

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url, re_path
+from django.conf.urls import include, url
+from django.urls import re_path
 from django.contrib import admin
 from django.views.generic import RedirectView
 
