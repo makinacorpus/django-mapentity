dea96505d96ae60434bfc7c55ab8525fdd0c2cda
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-11-18 13:43:19 +0100

    Remove 14.04 support

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 
 echo "--- Install Ubuntu packages:"
 sudo apt -y install python-virtualenv build-essential python3-dev libgdal-dev
-if [ "`lsb_release -rs`" = "14.04" ]; then
-    sudo apt -y install spatialite-bin
-else
-    sudo apt -y install libsqlite3-mod-spatialite
-fi
+sudo apt -y install libsqlite3-mod-spatialite
 sudo apt -y install libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
 echo "--- Create virtualenv:"
@@ -21,9 +17,7 @@ echo "--- Install python requirements:"
 echo "--- Install GDAL binding:"
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-if [ "`lsb_release -rs`" = "14.04" ]; then
-    ./env/bin/pip install gdal==1.10.0
-elif [ "`lsb_release -rs`" = "16.04" ]; then
+if [ "`lsb_release -rs`" = "16.04" ]; then
     ./env/bin/pip install gdal==1.11.2
 elif [ "`lsb_release -rs`" = "18.04" ]; then
     ./env/bin/pip install gdal==2.2.4
