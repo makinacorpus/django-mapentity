f5318d5972f1fa7978e626a1a764293344340d9f
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-12-10 11:48:45 +0100

    Use dummy and anygeom models for test_serializers

--- mapentity/tests/test_serializers.py
+++ mapentity/tests/test_serializers.py
@@ -14,28 +14,25 @@ from mapentity.serializers import ZipShapeSerializer, CSVSerializer
 from mapentity.serializers.shapefile import shape_write, info_from_geo_field, geo_field_from_model
 from mapentity.settings import app_settings
 
-from geotrek.core.models import Path
-from geotrek.core.factories import PathFactory
-from geotrek.diving.factories import DiveFactory
-from geotrek.diving.models import Dive, Difficulty
-from geotrek.common.factories import ThemeFactory
+from test_app.models import DummyModel, AnyGeomModel, Theme
+from test_app.factories import DummyModelFactory, AnyGeomModelFactory, TagFactory
 
 
 class ShapefileSerializer(TestCase):
     def setUp(self):
-        self.point1 = DiveFactory.create()
-        self.point1.themes.add(ThemeFactory.create(label="Tag1"))
-        self.point1.themes.add(ThemeFactory.create(label="Tag2"))
-        self.line1 = DiveFactory.create(geom='SRID=%s;LINESTRING(0 0, 10 0)' % settings.SRID)
-        self.multiline = DiveFactory.create(geom='SRID=%s;MULTILINESTRING((10 10, 20 20, 10 40),'
+        self.point1 = AnyGeomModelFactory.create()
+        self.point1.tags.add(TagFactory.create(label="Tag1"))
+        self.point1.tags.add(TagFactory.create(label="Tag2"))
+        self.line1 = AnyGeomModelFactory.create(geom='SRID=%s;LINESTRING(0 0, 10 0)' % settings.SRID)
+        self.multiline = AnyGeomModelFactory.create(geom='SRID=%s;MULTILINESTRING((10 10, 20 20, 10 40),'
                                                  '(40 40, 30 30, 40 20, 30 10))' % settings.SRID)
-        self.multipoint = DiveFactory.create(geom='SRID=%s;MULTIPOINT((1 1), (2 2))' % settings.SRID)
-        self.polygon = DiveFactory.create(geom='SRID=%s;POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))' % settings.SRID)
-        self.multipolygon = DiveFactory.create(geom='SRID=%s;MULTIPOLYGON(((0 0, 0 1, 1 1, 1 0, 0 0)),'
+        self.multipoint = AnyGeomModelFactory.create(geom='SRID=%s;MULTIPOINT((1 1), (2 2))' % settings.SRID)
+        self.polygon = AnyGeomModelFactory.create(geom='SRID=%s;POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))' % settings.SRID)
+        self.multipolygon = AnyGeomModelFactory.create(geom='SRID=%s;MULTIPOLYGON(((0 0, 0 1, 1 1, 1 0, 0 0)),'
                                                     '((2 2, 2 3, 3 3, 3 2, 2 2)))' % settings.SRID)
         self.serializer = ZipShapeSerializer()
         response = HttpResponse()
-        self.serializer.serialize(Dive.objects.all(), stream=response,
+        self.serializer.serialize(AnyGeomModel.objects.all(), stream=response,
                                   fields=['id', 'name'], delete=False)
 
     def getShapefileLayers(self):
@@ -104,39 +101,39 @@ class ShapefileSerializer(TestCase):
         self.serializer = ZipShapeSerializer()
         response = HttpResponse()
         with self.assertRaisesRegex(ValueError, "No geodjango geometry fields found in this model"):
-            self.serializer.serialize(Difficulty.objects.all(), stream=response,
+            self.serializer.serialize(Theme.objects.all(), stream=response,
                                       fields=['id', 'name'], delete=False)
 
     def test_serializer_model_geofield_multiple(self):
         app_settings['GEOM_FIELD_NAME'] = None
         self.serializer = ZipShapeSerializer()
-        PathFactory.create()
+        DummyModelFactory.create()
         response = HttpResponse()
         with self.assertRaisesRegex(ValueError, "More than one geodjango geometry field found, please specify which "
                                                 "to use by name using the 'geo_field' keyword. "
                                                 "Available fields are: 'geom_3d, geom, geom_cadastre'"):
-            self.serializer.serialize(Path.objects.all(), stream=response,
+            self.serializer.serialize(DummyModel.objects.all(), stream=response,
                                       fields=['id', 'name'], delete=False)
         app_settings['GEOM_FIELD_NAME'] = 'geom'
 
     def test_serializer_model_geofield_do_not_exist(self):
         app_settings['GEOM_FIELD_NAME'] = 'do_not_exist'
         self.serializer = ZipShapeSerializer()
-        PathFactory.create()
+        DummyModelFactory.create()
         response = HttpResponse()
         with self.assertRaisesRegex(ValueError, "Geodjango geometry field not found with the name 'do_not_exist', "
                                                 "fields available are: 'geom_3d, geom, geom_cadastre'"):
-            self.serializer.serialize(Path.objects.all(), stream=response,
+            self.serializer.serialize(DummyModel.objects.all(), stream=response,
                                       fields=['id', 'name'], delete=False)
         app_settings['GEOM_FIELD_NAME'] = 'geom'
 
     def test_serializer_shape_write_special_srid(self):
-        geo_field = geo_field_from_model(Dive, 'geom')
+        geo_field = geo_field_from_model(AnyGeomModel, 'geom')
         get_geom, geom_type, srid = info_from_geo_field(geo_field)
-        dives = [dive for dive in Dive.objects.all() if dive.geom.geom_type == 'Point']
+        anygeom_elements = [elt for elt in AnyGeomModel.objects.all() if elt.geom.geom_type == 'Point']
         with TemporaryDirectory(dir=app_settings['TEMP_DIR']) as tmp_directory:
             shape_write(tmp_directory,
-                        dives, Dive, ['id', 'name'], get_geom, 'Point', 2154, 3812)
+                        anygeom_elements, AnyGeomModel, ['id', 'name'], get_geom, 'Point', 2154, 3812)
             ds = DataSource(os.path.join(tmp_directory, 'Point.shp'))
 
         layer = ds[0]
@@ -147,9 +144,9 @@ class ShapefileSerializer(TestCase):
 
 class CSVSerializerTests(TestCase):
     def setUp(self):
-        self.point = DiveFactory.create(name="Test")
-        self.point.themes.add(ThemeFactory.create(label="Tag1"))
-        self.point.themes.add(ThemeFactory.create(label="Tag2"))
+        self.point = AnyGeomModelFactory.create(name="Test")
+        self.point.tags.add(TagFactory.create(label="Tag1"))
+        self.point.tags.add(TagFactory.create(label="Tag2"))
         self.serializer = CSVSerializer()
         self.stream = StringIO()
 
@@ -157,7 +154,7 @@ class CSVSerializerTests(TestCase):
         self.stream.close()
 
     def test_content(self):
-        self.serializer.serialize(Dive.objects.all(), stream=self.stream,
+        self.serializer.serialize(AnyGeomModel.objects.all(), stream=self.stream,
                                   fields=['id', 'name'], delete=False)
         self.assertEqual(self.stream.getvalue(),
                          ('ID,Name\r\n{},'
@@ -166,7 +163,7 @@ class CSVSerializerTests(TestCase):
     @override_settings(USE_L10N=True)
     def test_content_fr(self):
         translation.activate('fr')
-        self.serializer.serialize(Dive.objects.all(), stream=self.stream,
+        self.serializer.serialize(AnyGeomModel.objects.all(), stream=self.stream,
                                   fields=['id', 'name'], delete=False)
         self.assertEqual(self.stream.getvalue(),
                          ('ID,Nom\r\n{},'
