89517e04d43fa6a23eef35d15b38fd2056c07047
J-E Castagnede <j.e.castagnede@gmail.com>
2021-11-29 11:18:27 +0100

    flake8

--- test_app/tests/test_widgets.py
+++ test_app/tests/test_widgets.py
@@ -9,7 +9,9 @@ class HiddenGeometryWidgetTestCase(TestCase):
         """ Widget should transform geometry to API_SRID if geos object provided """
         widget = HiddenGeometryWidget()
         output = widget.render('geometry', Point(0, 0, srid=2154))
-        self.assertEqual(output, '<input type="hidden" name="geometry" value="SRID=4326;POINT (-1.363081210117898 -5.983856309208756)">')
+        self.assertEqual(output,
+                         '<input type="hidden" name="geometry" value="SRID=4326;'
+                         'POINT (-1.363081210117898 -5.983856309208756)">')
 
 
 class SelectMultipleWithPopTestCase(TestCase):
