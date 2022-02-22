09fcc463274e1359ab4442b336cf73f4cf18ec9d
J-E Castagnede <j.e.castagnede@gmail.com>
2022-02-22 17:27:30 +0100

    use new datatable format

--- mapentity/static/mapentity/mapentity.views.js
+++ mapentity/static/mapentity/mapentity.views.js
@@ -3,39 +3,87 @@ $(window).on('entity:view:list', function (e, data) {
      * Datatables
      * .......................
      */
-    MapEntity.mainDatatable = JQDataTable.init($('#objects-list'), null /* no load at startup */, {
-        // Hide pk column
-        aoColumnDefs: [ { "bVisible": false, "aTargets": [ 0 ] } ],
-        sDom: "tpf",
-        aaData: [],
-        iDeferLoading: 0,
-        iDisplayLength: 15,  // TODO: this is VERY ANNOYING ! I want to fill height !
-        // Enable cache
-        fnServerData: function ( sUrl, aoData, fnCallback, oSettings ) {
-			oSettings.jqXHR = $.ajax( {
-				"url":  sUrl,
-				"data": aoData,
-				"success": function (json) {
-					$(oSettings.oInstance).trigger('xhr', oSettings);
-					fnCallback( json );
-				},
-				"dataType": "json",
-				"cache": true,
-				"type": oSettings.sServerMethod,
-				"error": function (xhr, error, thrown) {
-					if ( error == "parsererror" ) {
-						oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
-							"server could not be parsed. This is caused by a JSON formatting error." );
-					}
-				}
-			} );
-		}
+    MapEntity.mainDatatable = $('#objects-list').DataTable({
+        'processing': true,
+        'serverSide': true,
+        aoColumnDefs: [
+            { "bVisible": false, "aTargets": [ 0 ] },  // don't show first column (ID)
+            {
+                "aTargets": [ 1 ],
+                "mRender": function ( column_data, type, full ) {  // render second column as detail link
+                    var value = '';
+                    if (column_data !== '') {
+                        value = column_data;
+                    } else {
+                        value = full.id.toString();
+                    }
+                    return `<a href="/${data.modelname}/${full.id}/">${value}</a>`;
+                }
+            }
+        ],  // hide id column (consider first)
+        "ajax": {
+            "url": `/api/${data.modelname}/drf/${data.modelname}s.datatables`,
+            "data": function (data){
+                //data.in_bbox= '-50,-25,-40,-10'
+            }
+        },
+        responsive: true,
+        pageLength: null, // page size is computed from the window size - expandDatatableHeight()
+        scrollY: '100vh',
+        scrollCollapse: true,
+        "lengthChange": false, // disable page length selection
+        "info": false, // hide infos "showinf xxx of xxx elements"
+        sDom: "tpf", // move search field at bottom left
+        "language": {
+            "searchPlaceholder": tr("Search"),  // placeholder in search field
+            "paginate": {
+                "first":      "<<",
+                "last":       ">>",
+                "next":       ">",
+                "previous":   "<"
+            },
+        }
     });
+    $("#objects-list_filter").addClass("d-none"); // hide search field
+
+    // champs de recherche custom
+    $('#object-list-search').keyup(function(){
+        MapEntity.mainDatatable.search($(this).val()).draw() ;
+    })
+
+    // MapEntity.mainDatatable = JQDataTable.init($('#objects-list'), null /* no load at startup */, {
+    //     // Hide pk column
+    //     aoColumnDefs: [ { "bVisible": false, "aTargets": [ 0 ] } ],
+    //     sDom: "tpf",
+    //     aaData: [],
+    //     iDeferLoading: 0,
+    //     iDisplayLength: 15,
+    //     // Enable cache
+    //     fnServerData: function ( sUrl, aoData, fnCallback, oSettings ) {
+	// 		oSettings.jqXHR = $.ajax( {
+	// 			"url":  sUrl,
+	// 			"data": aoData,
+	// 			"success": function (json) {
+	// 				$(oSettings.oInstance).trigger('xhr', oSettings);
+	// 				fnCallback( json );
+	// 			},
+	// 			"dataType": "json",
+	// 			"cache": true,
+	// 			"type": oSettings.sServerMethod,
+	// 			"error": function (xhr, error, thrown) {
+	// 				if ( error == "parsererror" ) {
+	// 					oSettings.oApi._fnLog( oSettings, 0, "DataTables warning: JSON data from "+
+	// 						"server could not be parsed. This is caused by a JSON formatting error." );
+	// 				}
+	// 			}
+	// 		} );
+	// 	}
+    // });
 
     // Adjust vertically
-    expandDatatableHeight(MapEntity.mainDatatable);
+    expandDatatableHeight();
     $(window).resize(function (e) {
-        expandDatatableHeight(MapEntity.mainDatatable);
+        expandDatatableHeight();
     });
 
 
@@ -58,9 +106,6 @@ $(window).on('entity:view:list', function (e, data) {
     });
 
     // Hardcore Datatables customizations
-    $('li.next a').html($('li.next a').html().replace('Next', ''));
-    $('li.prev a').html($('li.prev a').html().replace('Previous', ''));
-    $('#objects-list_filter input').attr('placeHolder', tr("Search")).addClass('form-control');
     $('#objects-list_filter label').contents().filter(function() {return this.nodeType === 3;/*Node.TEXT_NODE*/}).remove();
 
 });
