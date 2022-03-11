525bfbf3fd6a9706a4684663fb84ec62ed10cb2f
J-E Castagnede <j.e.castagnede@gmail.com>
2022-03-11 15:11:29 +0100

    fix mutable mapping

--- mapentity/serializers/datatables.py
+++ mapentity/serializers/datatables.py
@@ -8,8 +8,10 @@ class MapentityDatatableSerializer(ModelSerializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # patch mapping fields to use datatables format
-        self.serializer_field_mapping.update({
+        mappings = self.serializer_field_mapping.copy()
+        mappings.update({
             models.BooleanField: fields.MapentityDatatableBooleanField,
             models.DateTimeField: fields.MapentityDatatableDateTimeField,
             models.DateField: fields.MapentityDatatableDateField,
         })
+        self.serializer_field_mapping = mappings
