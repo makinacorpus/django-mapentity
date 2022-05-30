33e693337ab646eaccb724b9c4b3eb3352c6e412
J-E Castagnede <j.e.castagnede@gmail.com>
2022-05-30 17:40:56 +0200

    fix pagination

--- mapentity/pagination.py
+++ mapentity/pagination.py
@@ -3,9 +3,9 @@ from rest_framework_datatables.pagination import DatatablesPageNumberPagination
 
 class MapentityDatatablePagination(DatatablesPageNumberPagination):
     """ Custom datatable pagination for Mapentity list views. """
-
-    def get_count_and_total_count(self, queryset, view):
-        """ Handle count for all filters """
-        count, total_count = super().get_count_and_total_count(queryset, view)
-        count = queryset.count()  # replace count by real count - not only drf-datatables count
-        return count, total_count
+    pass
+    # def get_count_and_total_count(self, queryset, view):
+    #     """ Handle count for all filters """
+    #     count, total_count = super().get_count_and_total_count(queryset, view)
+    #     count = queryset.count()  # replace count by real count - not only drf-datatables count
+    #     return count, total_count
