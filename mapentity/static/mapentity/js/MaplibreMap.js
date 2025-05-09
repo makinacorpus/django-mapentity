e4b3b29abc603854126591c3c18d4a2bc5e20a53
kayarhabby <pasciahershekaya@gmail.com>
2025-05-09 18:09:02 +0200

    amélioration globale de la classe, ne gère plus que l'initialisation de la carte

--- mapentity/static/mapentity/js/MaplibreMap.js
+++ mapentity/static/mapentity/js/MaplibreMap.js
@@ -1,11 +1,8 @@
 class MaplibreMap {
-       constructor(id) {
+    constructor(id) {
         this.id = id;
         this.map = null;
-        this.layers = {
-            baseLayers: {},
-            overlays: {}
-        };
+        this.objectsLayer = null;
 
         this._init();
     }
@@ -17,12 +14,12 @@ class MaplibreMap {
             return;
         }
 
-        //  Initialisation de la carte
+        // Initialisation de la carte
         this.map = new maplibregl.Map({
             container: this.id,
             style: 'https://demotiles.maplibre.org/style.json',
-            center: [2.3522, 48.8566],
-            zoom: 8,
+            center: [1.4442, 43.6045],
+            zoom: 6,
         });
 
         // add base control
@@ -30,7 +27,7 @@ class MaplibreMap {
         this.map.addControl(new maplibregl.FullscreenControl(), 'top-left');
 
         // Création et ajout du contrôleur de mesure
-        const measureControl = new MeasureMaplibreControl();
+        const measureControl = new MaplibreMeasureControl();
         this.map.addControl(measureControl, 'top-left');
 
         // add scale control
@@ -39,70 +36,18 @@ class MaplibreMap {
             unit: 'metric'
         });
         this.map.addControl(scale, 'bottom-left');
-        // scale.setUnit('imperial'); // imperial ou metric, imperial -> miles, metric -> km
-
-    }
-
-    //  Ajoute un fond de carte raster (base layer)
-    addBaseLayer(name, layerConfig) {
-        const { id, tiles, tileSize = 256, attribution = '' } = layerConfig; // destructuration de layerConfig
-
-        this.map.addSource(id, {
-            type: 'raster',
-            tiles: tiles, // Exemple : ['https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png']
-            tileSize,
-            attribution
-        });
-
-        this.map.addLayer({
-            id,
-            type: 'raster',
-            source: id,
-            layout: { visibility: 'none' } // désactivé par défaut
-        });
-
-        this.layers.baseLayers[name] = id;
-    }
-
-    //  Ajoute un calque GeoJSON (overlay), avec catégorie
-    addOverlay(name, geojsonData, category = 'Autres') {
-        const sourceId = `overlay-${name}`;
-        const layerId = `layer-${name}`;
-
-        this.map.addSource(sourceId, {
-            type: 'geojson',
-            data: geojsonData
-        });
-
-        this.map.addLayer({
-            id: layerId,
-            type: 'fill',
-            source: sourceId,
-            layout: {},
-            paint: {
-                'fill-color': '#088',
-                'fill-opacity': 0.5
-            }
-        });
-
-        if (!this.layers.overlays[category]) {
-            this.layers.overlays[category] = {};
-        }
-        this.layers.overlays[category][name] = layerId;
-
-    }
-
-    // Méthode utilitaire pour changer la visibilité
-    toggleLayer(layerId, visible = true) {
-        this.map.setLayoutProperty(layerId, 'visibility', visible ? 'visible' : 'none');
     }
 
     getMap() {
         return this.map;
     }
 
-    getLayers() {
-        return this.layers;
+   setObjectsLayer(objectsLayer) {
+        this.objectsLayer = objectsLayer;
+        this.objectsLayer.initialize(this.map); // Initialiser manuellement la couche
     }
 
+    getObjectsLayer() {
+        return this.objectsLayer;
+    }
 }
