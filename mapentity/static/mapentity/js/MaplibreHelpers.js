f25a92487dfeb34d137004ab6677ce82d0c359d4
kayarhabby <pasciahershekaya@gmail.com>
2025-05-15 17:36:18 +0200

    update : mise en commentaire des méthodes qui ne peuvent être utilisées pour le moment

---
+++ mapentity/static/mapentity/js/MaplibreHelpers.js
@@ -0,0 +1,117 @@
+// Toggable console.debug() function
+console.debug = function () {
+    if (window.SETTINGS && window.SETTINGS.debug) {
+        if (arguments.length > 1)
+            console.log(arguments);
+        else
+            console.log(arguments[0]);
+    }
+};
+
+/**
+ * Get URL parameter in Javascript
+ * @param name {string} - The name of the parameter to retrieve from the URL
+ * @returns {string} - The value of the parameter, or null if not found
+ */
+function getURLParameter(name) {
+    return new URLSearchParams(window.location.search).get(name);
+}
+
+/**!
+ * @preserve parseColor
+ * Copyright 2011 THEtheChad Elliott
+ * Released under the MIT and GPL licenses.
+ */
+parseColor = function(color) {
+
+    var cache
+      , p = parseInt // Use p as a byte saving reference to parseInt
+      , color = color.replace(/\s\s*/g,'') // Remove all spaces
+    ;
+
+    // Checks for 6 digit hex and converts string to integer
+    if (cache = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/.exec(color))
+        cache = [p(cache[1], 16), p(cache[2], 16), p(cache[3], 16)];
+
+    // Checks for 3 digit hex and converts string to integer
+    else if (cache = /^#([\da-fA-F])([\da-fA-F])([\da-fA-F])/.exec(color))
+        cache = [p(cache[1], 16) * 17, p(cache[2], 16) * 17, p(cache[3], 16) * 17];
+
+    // Checks for rgba and converts string to
+    // integer/float using unary + operator to save bytes
+    else if (cache = /^rgba\(([\d]+),([\d]+),([\d]+),([\d]+|[\d]*.[\d]+)\)/.exec(color))
+        cache = [+cache[1], +cache[2], +cache[3], +cache[4]];
+
+    // Checks for rgb and converts string to
+    // integer/float using unary + operator to save bytes
+    else if (cache = /^rgb\(([\d]+),([\d]+),([\d]+)\)/.exec(color))
+        cache = [+cache[1], +cache[2], +cache[3]];
+
+    // Otherwise throw an exception to make debugging easier
+    else throw Error(color + ' is not supported by parseColor');
+
+    // Performs RGBA conversion by default
+    isNaN(cache[3]) && (cache[3] = 1);
+
+    // Adds or removes 4th value based on rgba support
+    // Support is flipped twice to prevent erros if
+    // it's not defined
+    return cache.slice(0,3 + !!$.support.rgba);
+};
+
+function expandDatatableHeight() {
+    // Calculate the available height for the table by subtracting 75 from the container height
+    var fill_height = $('#objects-list_wrapper').height() - 75;
+    // Define the height of a single table row
+    var row_height = 36;
+    // Calculate the number of rows that can fit in the available space
+    var number_of_rows = Math.floor(fill_height / row_height);
+    // Update the number of rows displayed in the table and redraw
+    $('#objects-list').DataTable().page.len(parseInt(number_of_rows.toString())).draw();
+}
+
+// translation langue
+// function tr(s) {
+//     return MapEntity.i18n[s] || s;
+// }
+
+//cette fonction personnalise TinyMCE pour gérer les limites de caractères et améliorer
+// l'expérience utilisateur avec des validations visuelles.
+// ceci pourrait être réécrit en js ou gardé tel quel pour garantir la compatibilité
+function tinyMceInit(editor) {
+    var context = $('body').data();
+    editor.on('WordCountUpdate', function(event) {
+        console.log(window.SETTINGS);
+        // DEPRECATED paramters maxCharacters -> to remove
+        if (("container" in event.target) && (window.SETTINGS.maxCharacters > 0)) {
+            var characters = event.wordCount.characters;
+            if (characters > window.SETTINGS.maxCharacters) {
+                event.target.container.classList.add('cec-overflow');
+            } else {
+                event.target.container.classList.remove('cec-overflow');
+            }
+        }
+        if (("container" in event.target) && (window.SETTINGS.maxCharactersByField)) {
+            var fullTableName = context.appname+"_"+context.modelname
+            if (fullTableName in window.SETTINGS.maxCharactersByField) {
+                var currenInputName = event.target.container.previousSibling.name;
+                window.SETTINGS.maxCharactersByField[fullTableName].forEach(config => {
+                    if(config.field == currenInputName) {
+                        var statusBar = $(event.target.container).find(".tox-statusbar__wordcount");
+                        $(event.target.container).find(".injectedCount").remove()
+                        $("<p class='injectedCount'>"+event.wordCount.characters+"/"+config.value+" characters</p>").insertBefore(statusBar)
+                        if(event.wordCount.characters > config.value) {
+
+                            event.target.container.classList.add('cec-overflow');
+                            event.target.container.classList.add('is-invalid');
+                        } else {
+                            event.target.container.classList.remove('cec-overflow');
+                            event.target.container.classList.remove('is-invalid');
+                        }
+                    }
+                })
+            }
+        }
+    });
+}
+
---
+++ mapentity/static/mapentity/js/MaplibreMapListSync.js
@@ -0,0 +1,223 @@
+class MaplibreMapListSync {
+    constructor(datatables, map, objectsLayer, options) {
+        this.dt = datatables;
+        this.map = map;
+        this.layer = objectsLayer;
+        this.options = options || {};
+
+        this.selectorOnce = this.__initSelectorOnce();
+
+        // this.layer.on('mouseenter', this._onObjectOver.bind(this));
+
+        // this._loading = false; // loading state
+
+        // this.map.on('moveend', this._onMapViewChanged.bind(this));
+
+    }
+
+    // _onListFilter() {
+    //     var filterTxt = $(".dataTables_filter input[type='text']").val();
+    //     var results = this.dt.fnGetColumnData(0);
+    //     this.fire('reloaded', {
+    //         nbrecords: results.length,
+    //     });
+    //     this.layer.updateFromPks(results);
+    // }
+
+    // _onMapViewChanged() {
+    //     if (!this.map.loaded()) {
+    //         setTimeout(() => this.map.fire('moveend'), 20);
+    //         return;
+    //     }
+    //     this._formSetBounds();
+    //     this._reloadList();
+    // }
+
+    // _onFormSubmit(e) {
+    //     this._formSetBounds();
+    //     this._reloadList(true);
+    // }
+    //
+    // _onFormReset(e) {
+    //     this._formClear($(this.options.filter.form));
+    //     this._reloadList();
+    //     this._formSetBounds();
+    // }
+
+    // _onObjectOver(e) {
+    //     var search_pk = e.layer.properties.pk;
+    // }
+
+    _onRowCreated(nRow, aData, iDataIndex) {
+        var self = this;
+        var pk = aData[0];
+        $(nRow).hover(
+            function() {
+                self.layer.highlight(pk);
+            },
+            function() {
+                self.layer.highlight(pk, false);
+            }
+        );
+
+        $(nRow).click(function() {
+            self.selectorOnce.select(pk, $(nRow));
+        });
+
+        // $(nRow).dblclick(function() {
+        //     self.layer.jumpTo(pk);
+        // });
+    }
+
+    // _reloadList(refreshLayer) {
+    //     var formData = new FormData(document.querySelector('#mainfilter'));
+    //     var filter = false;
+    //
+    //     for (var value of Array.from(formData)) {
+    //         if (value[0] !== 'bbox') {
+    //             if (value[1] !== '') {
+    //                 filter = true;
+    //             }
+    //         }
+    //     }
+    //
+    //     if (filter) {
+    //         $('#filters-btn').removeClass('btn-info');
+    //         $('#filters-btn').addClass('btn-warning');
+    //     } else {
+    //         $('#filters-btn').removeClass('btn-warning');
+    //         $('#filters-btn').addClass('btn-info');
+    //     }
+    //
+    //     this.dt.ajax.url($('#mainfilter').attr('action') + '?' + $('#mainfilter').serialize()).load();
+    //
+    //     if (this._loading)
+    //         return;
+    //     this._loading = true;
+    //
+    //     var self = this;
+    //
+    //     var extract_data_and_pks = function(data, type, callback_args) {
+    //         callback_args.map_obj_pk = data.map_obj_pk;
+    //         return data.aaData;
+    //     };
+    //
+    //     var on_data_loaded = function(oSettings, callback_args) {
+    //         var nbrecords = self.dt.fnSettings().fnRecordsTotal();
+    //         var nbonmap = Object.keys(self.layer.getCurrentLayers()).length;
+    //
+    //         if (refreshLayer || (nbrecords > nbonmap)) {
+    //             var updateLayerObjects = function() {
+    //                 self.layer.updateFromPks(callback_args.map_obj_pk);
+    //                 self._onListFilter();
+    //             };
+    //
+    //             if (self.layer.loading) {
+    //                 self.layer.on('loaded', updateLayerObjects);
+    //             } else {
+    //                 setTimeout(updateLayerObjects, 0);
+    //             }
+    //         }
+    //
+    //         self.fire('reloaded', {
+    //             nbrecords: nbrecords,
+    //         });
+    //
+    //         self._loading = false;
+    //     };
+    //
+    //     $.get($('#mainfilter').attr('action').replace('.datatables', '/filter_infos.json'),
+    //         $('#mainfilter').serialize(),
+    //         function(data) {
+    //             $('#nbresults').text(data.count);
+    //             this.layer.updateFromPks(data.pk_list);
+    //             self._loading = false;
+    //         }.bind(this));
+    //
+    //     var url = this.options.url;
+    //     if (this.options.filter) {
+    //         url = this.options.filter.form.attr("action") + '?' + this.options.filter.form.serialize();
+    //     }
+    //     return false;
+    // }
+
+    // _formSetBounds() {
+    //     if (!this.options.filter)
+    //         return;
+    //
+    //     if (!this.map.loaded()) {
+    //         console.warn("Map view not set, cannot get bounds.");
+    //         return;
+    //     }
+    //
+    //     var bounds = this.map.getBounds();
+    //     var min_lat = Math.max(bounds._sw.lat, -90);
+    //     var max_lat = Math.min(bounds._ne.lat, 90);
+    //     var min_lon = Math.max(bounds._sw.lng, -180);
+    //     var max_lon = Math.min(bounds._ne.lng, 180);
+    //
+    //     // Assuming you have a method to convert bounds to WKT or another format for your filter
+    //     var bboxString = `${min_lon},${min_lat},${max_lon},${max_lat}`;
+    //     this.options.filter.bboxfield.val(bboxString);
+    // }
+
+    // _formClear($form) {
+    //     $form.find('input:text, input:password, input:file, input[type=number], select, textarea').val('').trigger('change');
+    //     $form.find('input:radio, input:checkbox, select option')
+    //          .removeAttr('checked').removeAttr('selected');
+    //     $form.find('select').val('').trigger("chosen:updated");
+    // }
+
+    __initSelectorOnce() {
+        var self = this;
+        var selectorOnce = (function() {
+            var current = { 'pk': null, 'row': null };
+
+            function toggleSelectRow($prevRow, $nextRow) {
+                function nextRowAnim() {
+                    if ($nextRow) {
+                        $nextRow.hide('fast')
+                                .show('fast', function() { $nextRow.addClass('success'); });
+                    }
+                }
+
+                if ($prevRow) {
+                    $prevRow.hide('fast', function() { $prevRow.removeClass('success'); })
+                            .show('fast', nextRowAnim);
+                } else {
+                    nextRowAnim();
+                }
+            }
+
+            function toggleSelectObject(pk, on) {
+                on = on === undefined ? true : on;
+                self.layer.select(pk, on);
+            }
+
+            return {
+                'select': function(pk, row) {
+                    if (pk == current.pk) {
+                        pk = null, row = null;
+                    }
+
+                    var prev = current;
+                    current = {'pk': pk, 'row': row};
+
+                    toggleSelectRow(prev.row, row);
+
+                    if (prev.pk && prev.row) {
+                        toggleSelectObject(prev.pk, false);
+                    }
+                    if (row && pk) {
+                        toggleSelectObject(pk, true);
+                    }
+                }
+            };
+        })();
+        return selectorOnce;
+    }
+
+    fire(event, data) {
+        console.log(`Event fired: ${event}`, data);
+    }
+}
