72afe1f48ef502220a53045f476b181bd706992d
kayarhabby <pasciahershekaya@gmail.com>
2025-07-31 15:16:34 +0200

    ajout de l'attribut modifiable et prise en compte de target_map

--- mapentity/widgets.py
+++ mapentity/widgets.py
@@ -2,19 +2,9 @@ from django.forms import widgets as django_widgets
 from django.template.loader import render_to_string
 from .helpers import wkt_to_geom
 from .settings import API_SRID
-
 from django.core import validators
 from django.template.defaultfilters import slugify
 from django.contrib.gis.forms.widgets import BaseGeometryWidget
-
-# class MapWidget(MaplibreWidget):
-#     # geometry_field_class = 'MapEntity.GeometryField'
-#
-#     def render(self, name, value, attrs=None, renderer=None):
-#         # attrs = attrs or {}
-#         # attrs.update(geometry_field_class=self.geometry_field_class)
-#         return super().render(name, value, attrs)
-
 
 class MapWidget(BaseGeometryWidget):
     """
@@ -22,6 +12,7 @@ class MapWidget(BaseGeometryWidget):
     """
     template_name = 'mapentity/widget.html'
     display_raw = False
+    modifiable = True
 
     def serialize(self, value):
         """
@@ -35,25 +26,22 @@ class MapWidget(BaseGeometryWidget):
         """
         # Récupération des paramètres depuis l'initialisation du Field
         self.geom_type = self.attrs.get('geom_type', getattr(self, 'geom_type', 'GEOMETRY'))
-
         attrs = attrs or {}
-
         # Normalisation du type de géométrie
         if self.geom_type == 'GEOMETRY':
             attrs['geom_type'] = 'Geometry'
         else:
             attrs['geom_type'] = self.geom_type
-
         # Génération des IDs pour les éléments HTML et JavaScript
         map_id_css = slugify(attrs.get('id', name))
         map_id = map_id_css.replace('-', '_')
-
         attrs.update({
             'id': map_id,
             'id_css': map_id_css,
             'id_map': map_id_css + '_map',
+            'modifiable': self.modifiable,
+            'target_map': attrs.get('target_map', getattr(self, 'target_map', None)),
         })
-
         return attrs
 
     def get_context(self, name, value, attrs):
@@ -62,21 +50,16 @@ class MapWidget(BaseGeometryWidget):
         """
         # Gestion des valeurs vides
         value = None if value in validators.EMPTY_VALUES else value
-
         # Récupération du contexte parent
         context = super().get_context(name, value, attrs)
-
         # Ajout des attributs spécifiques au widget
         widget_attrs = self._get_attrs(name, attrs)
         context.update(widget_attrs)
-
         # Ajout de la valeur sérialisée pour le template
         context['serialized'] = self.serialize(value)
-
         return context
 
 class HiddenGeometryWidget(django_widgets.HiddenInput):
-
     def value_from_datadict(self, data, files, name):
         """
         From WKT to Geometry (TODO: should be done in Field clean())
@@ -91,7 +74,6 @@ class HiddenGeometryWidget(django_widgets.HiddenInput):
         if value and not isinstance(value, str):
             value.transform(API_SRID)
         return value
-
 
 class SelectMultipleWithPop(django_widgets.SelectMultiple):
     def __init__(self, *args, **kwargs):
