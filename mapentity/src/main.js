723d1e03af6c0b3b3477306992bf698ac8f078e0
J-E Castagnede <j.e.castagnede@gmail.com>
2025-09-09 17:24:08 +0200

    rearrange dist files

--- mapentity/src/main.js
+++ mapentity/src/main.js
@@ -2,7 +2,9 @@ import jQuery from 'jquery';
 
 import 'jquery-hoverintent';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'popper.js';
+
 require('select2')(window, jQuery);
 import 'bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.fr.min.js';
@@ -10,5 +12,11 @@ import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.it.min.js';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.de.min.js';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.nl.min.js';
+
 require('datatables.net/js/jquery.dataTables.js')(window, jQuery);
 require('datatables.net-bs4/js/dataTables.bootstrap4.js')(window, jQuery);
+
+import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'select2/dist/css/select2.css';
