64350508b3514391553219d57f91c39f6c1cb38d
J-E Castagnede <j.e.castagnede@gmail.com>
2025-11-06 11:47:23 +0100

    use bootsrap classes

--- test_project/test_app/filters.py
+++ test_project/test_app/filters.py
@@ -1,12 +1,8 @@
-from django_filters import rest_framework as rest_filters
-
 from mapentity.filters import MapEntityFilterSet
 from test_project.test_app.models import DummyModel, Road
 
 
 class DummyModelFilterSet(MapEntityFilterSet):
-    public = rest_filters.BooleanFilter(field_name="public", lookup_expr="exact")
-
     class Meta(MapEntityFilterSet.Meta):
         model = DummyModel
         fields = ("public", "name", "tags")
