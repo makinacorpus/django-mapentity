4ca26c05488b579de050a67ef355d4f1e8089413
J-E Castagnede <j.e.castagnede@gmail.com>
2025-11-14 16:32:43 +0100

    add FK field and change select

--- mapentity/forms.py
+++ mapentity/forms.py
@@ -103,7 +103,7 @@ class MapEntityForm(TranslatedModelForm):
     hidden_fields = []
 
     default_widgets = {
-        models.ForeignKey: autocomplete.Select2,
+        models.ForeignKey: autocomplete.ListSelect2,
         models.ManyToManyField: autocomplete.Select2Multiple,
     }
 
