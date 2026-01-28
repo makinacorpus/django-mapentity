e5259fc4fdb40c57ba9e2988584efdc71901f4cf
PartyNell <nell.party@makina-corpus.com>
2026-01-28 14:57:04 +0100

    Fix tests

--- test_app/tests/test_views.py
+++ test_app/tests/test_views.py
@@ -789,7 +789,7 @@ class LogViewMapentityTest(MapEntityTest):
             "change_message": "",
             "content_type": str(content_type),
             "id": 1,
-            "object": '<a data-pk="1" href="/dummymodel/1/" >Test_App | Dummy '
+            "object": '<a data-pk="1" href="/dummymodel/1/" >Test App | Dummy '
             "Model <class 'object'></a>",
             "object_id": "1",
             "object_repr": "<class 'object'>",
