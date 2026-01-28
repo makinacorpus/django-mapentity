5a42be56bcbc66e4843c08aca47d290fbfd213c7
J-E Castagnede <j.e.castagnede@gmail.com>
2026-01-28 09:55:22 +0100

    Add PostgreSQL database configuration to demo settings

--- test_project/settings/demo.py
+++ test_project/settings/demo.py
@@ -3,3 +3,14 @@ from . import *  # NOQA
 DEBUG = False
 
 ALLOWED_HOST = os.getenv("SERVER_NAME", "").split(",")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
