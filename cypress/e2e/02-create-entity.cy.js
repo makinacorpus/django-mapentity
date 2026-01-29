149a8fdf46282c42789ba59f2bc263c42b2cd294
Jean-Etienne Castagnede <j.e.castagnede@gmail.com>
2026-01-29 22:29:56 +0000

    fix tests

--- cypress/e2e/02-create-entity.cy.js
+++ cypress/e2e/02-create-entity.cy.js
@@ -14,6 +14,10 @@ describe('DummyModel Create', () => {
     cy.get('.maplibre-map, #mainmap, .map-panel, [id*="map"]', { timeout: 10000 }).should('exist')
   })
 
+  it('should have draw marker button', () => {
+    cy.get('#id_draw_marker', { timeout: 10000 }).should('exist')
+  });
+
   it('should show validation error when submitting without geometry', () => {
     // Fill only text fields without drawing geometry
     cy.get('input[name="name_en"]', { timeout: 10000 }).type('Test Entity Without Geom')
@@ -44,39 +48,10 @@ describe('DummyModel Create', () => {
     
     // Wait a bit for Geoman to initialize
     cy.wait(2000)
+
+    cy.get('#id_draw_marker').click();
     
-    // Try to find and click the marker/point draw button
-    // Geoman typically adds buttons with specific data attributes or classes
-    cy.get('body').then($body => {
-      // Look for common Geoman draw control selectors
-      const selectors = [
-        '[data-gm-tool="drawMarker"]',
-        '.leaflet-pm-icon-marker',
-        'button[title*="marker" i]',
-        'button[title*="point" i]',
-        '.maplibregl-ctrl button:first',
-        '.mapboxgl-ctrl-group button:first'
-      ]
-      
-      let buttonFound = false
-      for (const selector of selectors) {
-        if ($body.find(selector).length > 0) {
-          cy.get(selector).first().click({ force: true })
-          buttonFound = true
-          break
-        }
-      }
-      
-      if (!buttonFound) {
-        // If no draw button found, try clicking on the map directly
-        cy.log('No draw button found, clicking map directly')
-        cy.get('.maplibre-map, [id*="map"]').first().click(200, 200, { force: true })
-      } else {
-        // After clicking draw button, click on the map to place the point
-        cy.wait(500)
-        cy.get('.maplibre-map, [id*="map"]').first().click(200, 200, { force: true })
-      }
-    })
+    cy.get('.maplibre-map, [id*="map"]').first().click(200, 200, { force: true })
     
     // Wait a bit for geometry to be registered
     cy.wait(1000)
