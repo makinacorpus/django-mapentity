eb31c1338b696ca2d02817807f4b6d9f6111012a
Jean-Etienne Castagnede <j.e.castagnede@gmail.com>
2026-01-29 23:56:38 +0000

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -10,8 +10,12 @@ module.exports = defineConfig({
     screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chromium') {
+        if (browser.name === 'electron') {
+          // Active SwiftShader pour Electron
+          launchOptions.preferences = launchOptions.preferences || {}
           launchOptions.args.push('--enable-unsafe-swiftshader')
+          launchOptions.args.push('--disable-gpu') // optionnel mais stable
+          launchOptions.args.push('--disable-dev-shm-usage') // utile en CI
         }
         return launchOptions
       })
