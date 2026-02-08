c6e44ebd8ece9da077162aeb5fe7fc2dc0023d86
J-E Castagnede <j.e.castagnede@gmail.com>
2026-02-08 14:38:03 +0100

    Add command to install OSM baselayer in E2E serve process

--- mapentity/apps.py
+++ mapentity/apps.py
@@ -1,9 +1,6 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.core.checks import Warning, register
-from django.db.models.signals import post_migrate
-
-from mapentity.signals import migrate_tiles
 
 
 class MapEntityConfig(AppConfig):
@@ -12,7 +9,7 @@ class MapEntityConfig(AppConfig):
 
     def ready(self):
         pass
-        #post_migrate.connect(migrate_tiles, sender=self)
+        # post_migrate.connect(migrate_tiles, sender=self)
 
 
 @register()
@@ -23,7 +20,6 @@ def old_config(app_configs, **kwargs):
                 """Warning: LEAFLET_CONFIG is defined in settings."""
                 """If you want to use newest MapEntity version, please make sure to report your LEAFLET_CONFIG to new MAPLIBRE_CONFIG."""
                 """TILES and OVERLAYS will be migrate automatically in new database section""",
-
                 id="mapentity.W001",
             )
         ]
