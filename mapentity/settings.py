9e1876e442f5030b7011a0a3ce43160e4350ccfd
J-E Castagnede <j.e.castagnede@gmail.com>
2026-02-13 13:33:01 +0100

    Update map icons to use SVG files from static resources

--- mapentity/settings.py
+++ mapentity/settings.py
@@ -14,7 +14,7 @@ _DEFAULT_MAP_STYLES = {
         "opacity": 1,
         "line-cap": "round",
         "color": "mediumpurple",
-        "arrowColor": "#000000",
+        "arrowColor": "#0000FF",
         "arrowSize": 0.5,
         "arrowOpacity": 1,
         "arrowSpacing": 20,
