b6bb72827b0376583fb8478a8885e0968e18f20a
PartyNell <nell.party@makina-corpus.com>
2026-02-24 11:49:43 +0100

    Fix migrations

--- test_project/test_app/migrations/0032_road_tag.py
+++ test_project/test_app/migrations/0032_road_tag.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("test_app", "0026_delete_supermarket"),
+        ("test_app", "0031_multigeommodel_date_update"),
     ]
 
     operations = [
